6120808bffe4f2ce140daf04eed2af68e17fef79
David Johnson <djohnson.m@gmail.com>
2019-12-01 00:21:40 -0500

    Use unfoldr, refactor to folder named 1.

--- 1/1.hs
+++ 1/1.hs
@@ -1,19 +1,23 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 module Main where
 
+import Data.List
+
 main :: IO ()
 main = do
   input :: [Int] <- fmap read . lines <$> readFile "input.txt"
-  -- part 1
-  print $ sum (fmap f input)
-  -- part 2
-  print $ sum (fmap (sum . accum) input)
+  -- Part 1
+  print $ sum (f <$> input)
+  -- Part 2
+  print $ sum (accum <$> input)
 
 -- | Part 1
 f :: Int -> Int
 f mass = floor (fromIntegral mass / 3) - 2
 
 -- | Part 2
-accum :: Int -> [Int]
-accum x | f x <= 0 = []
-        | otherwise = f x : accum (f x)
+accum :: Int -> Int
+accum = sum . unfoldr go
+  where
+    go x | f x <= 0  = Nothing
+         | otherwise = Just (x, f x)
